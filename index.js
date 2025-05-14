0d5d1630b5c0b550c3b5a472a7f5c2fa93d6397a
anderson purica <puricaanderson00@gmail.com>
2025-05-14 15:39:58 -0400

    solo falta un poco de Javascript

---
+++ index.js
@@ -0,0 +1,7 @@
+
+function myFunction() {
+    document.getElementById("demo1").innerHTML = "Home";
+    document.getElementById("demo2").innerHTML = "Proyects";
+    document.getElementById("demo3").innerHTML = "About";
+    document.getElementById("demo4").innerHTML = "Contact";
+}
